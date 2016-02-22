23dd57edb6b023a31241009d44825008a5a06d32
Sarath Kumar M <goldenvalley4ever@gmail.com>
2016-02-22 17:15:19 +0530

    Changes to tpl file.

--- simpleads-statsprint.tpl.php
+++ simpleads-statsprint.tpl.php
@@ -1,3 +1,51 @@
 <?php
-dpm($logo);
-dpm($data);
+$index = count($data[1]);
+?>
+<h2>Total</h2>
+<table>
+			<tr>
+				<th>Clicks</th>
+				<th>Unique clicks</th>
+				<th>Impressions</th>
+				<th>Unique impressions</th>
+				<th>Click-through rate</th>
+			</tr>
+			<tr>
+				<td><?php print $data[0]['click']; ?></td>
+				<td><?php print $data[0]['unique_clicks']; ?></td>
+				<td><?php print $data[0]['impressions']; ?></td>
+				<td><?php print $data[0]['unique_impressions']; ?></td>
+				<td><?php print 
+            ($data[0]['unique_clicks']/$data[0]['unique_impressions']) * 100;
+          ?></td>
+			</tr>							
+	</table>
+<h2>Daily statistics</h2>
+<table>
+  <tr>
+    <th>Date</th>
+    <th>Clicks</th>
+    <th>Unique Clicks</th>
+    <th>Impressions</th>
+    <th>Unique Impressions</th>
+  </tr>
+  <?php for ($i = 0; $i < $index; $i++): ?>
+    <tr>
+      <td>
+        <?php print date('d/m/y H:i A', $data[$i]['date']); ?>
+      </td>
+      <td>
+        <?php print $data[$i]['click']; ?>
+      </td>
+      <td>
+        <?php print $data[$i]['unique_clicks']; ?>
+      </td>
+      <td>
+        <?php print $data[$i]['impressions']; ?>
+      </td>
+      <td>
+        <?php print $data[$i]['unique_impressions']; ?>
+      </td>
+    </tr>
+  <?php endfor; ?>
+</table>
